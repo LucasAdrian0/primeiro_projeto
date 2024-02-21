8e6eee99d99131094e6545204848302ee3a6b1ba
Lucas Adriano <lukasadriano94@hotmail.com>
2024-02-21 23:50:22 +0000

    Classes

--- primeiro_exemplo/src/main/java/primeiro_exemplo/Ola.java
+++ primeiro_exemplo/src/main/java/primeiro_exemplo/Ola.java
@@ -1,8 +1,29 @@
 package primeiro_exemplo;
 
+import modelos.Pessoa;
+import modelos.PessoaFisica;
+
 public class Ola {
     public static void main (String[] args){
-        System.out.println("Funciona");
+        System.out.println("Funciona!!!");
+
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Marco");
+        System.out.println(p1.getNome());
+
+
+
+        PessoaFisica p2 = new PessoaFisica();
+        p2.setNome("Marco");
+        p2.setIdade(43);
+
+        System.out.println(p2.getNome());
+        System.out.println(p2.getIdade());
+
+
+
+
+
 
     }
 }
